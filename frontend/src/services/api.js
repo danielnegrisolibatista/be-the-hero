55f8ee3255065982d83a56d10dd0ded2260445b7
Daniel Negrisoli Batista <danielnegrisoli@gmail.com>
2020-04-01 15:51:55 -0300

    Atualização nos arquivos de configuração

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,17 @@
 import axios from 'axios';
 
+let url = 'http://localhost:3333'
+
+if(process.env.NODE_ENV === 'development') {
+  url = 'http://localhost:3333'
+}
+
+if(process.env.NODE_ENV === 'production') {
+  url = 'https://omnistack-be-the-hero.herokuapp.com'
+}
+
 const api = axios.create({
-    baseURL: 'http://localhost:3333'
+    baseURL: url
 })
 
 export default api;
